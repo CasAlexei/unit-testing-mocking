7d4bfd4d1a1e02cbf84a183add21e6c410fd0b69
CasAlexei <hp81@mail.ru>
2021-04-06 09:23:36 +0300

    create new branch dev

--- src/test/java/com/endava/internship/mocking/service/PaymentServiceTest.java
+++ src/test/java/com/endava/internship/mocking/service/PaymentServiceTest.java
@@ -1,14 +1,24 @@
 package com.endava.internship.mocking.service;
 
+import com.endava.internship.mocking.repository.PaymentRepository;
+import com.endava.internship.mocking.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 class PaymentServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private PaymentRepository paymentRepository;
+    @Mock
+    private ValidationService validationService;
 
 
     @BeforeEach
